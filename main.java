28530d9e3547f67b0a1b2512f265baa9bf124ba4
Ed Hunter <edhntr@gmail.com>
2013-09-03 20:49:08 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+
+public class main
+{
+	public static void main(String[] args)
+	{
+		bowling game  = new bowling();
+		game.scoring();
+		System.out.print("The total score is " + game.totalScore + ".");
+	}
+}
